d2309dd77a61260b231f638d1982def67400d7ba
Maksim-Artjomov <120183144+Maksim-Artjomov@users.noreply.github.com>
2023-09-15 10:49:18 +0300

    Update Game.cs

--- Game.cs
+++ Game.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -7,14 +7,14 @@ using System.Threading.Tasks;
 namespace Jalgpall
 {
 
-    public class Game
+    public class Game 
     {
         public Team HomeTeam { get; }
         public Team AwayTeam { get; }
         public Stadium Stadium { get; }
         public Ball Ball { get; private set; }
 
-        public Game(Team homeTeam, Team awayTeam, Stadium stadium)
+        public Game(Team homeTeam, Team awayTeam, Stadium stadium) // Конструктор для создания игры, где выдаём названия двум командам и название стадиону
         {
             HomeTeam = homeTeam;
             homeTeam.Game = this;
@@ -23,28 +23,28 @@ namespace Jalgpall
             Stadium = stadium;
         }
 
-        public void Start()
+        public void Start() // Метод для запуска игры
         {
-            Ball = new Ball(Stadium.Width / 2, Stadium.Height / 2, this);
-            HomeTeam.StartGame(Stadium.Width / 2, Stadium.Height);
-            AwayTeam.StartGame(Stadium.Width / 2, Stadium.Height);
+            Ball = new Ball(Stadium.Width / 2, Stadium.Height / 2, this); // Установка мяча на середину поля
+            HomeTeam.StartGame(Stadium.Width / 2, Stadium.Height); // Установка первой команды
+            AwayTeam.StartGame(Stadium.Width / 2, Stadium.Height); // Установка второй команды
         }
         private (double, double) GetPositionForAwayTeam(double x, double y)
         {
-            return (Stadium.Width - x, Stadium.Height - y);
+            return (Stadium.Width - x, Stadium.Height - y); // Вычисляем абсолютные координаты игрока на поле
         }
 
-        public (double, double) GetPositionForTeam(Team team, double x, double y)
+        public (double, double) GetPositionForTeam(Team team, double x, double y) // Возвращение абсолютных координат игрока на поле для заданной команды
         {
-            return team == HomeTeam ? (x, y) : GetPositionForAwayTeam(x, y);
+            return team == HomeTeam ? (x, y) : GetPositionForAwayTeam(x, y); // Если у HomeTeam координаты остаются без изменени, то используется GetPositionForAwayTeam
         }
 
-        public (double, double) GetBallPositionForTeam(Team team)
+        public (double, double) GetBallPositionForTeam(Team team) // Возвращение абсолютных координат мяча на поле для заданной команды, используя GetPositionForTeam
         {
             return GetPositionForTeam(team, Ball.X, Ball.Y);
         }
 
-        public void SetBallSpeedForTeam(Team team, double vx, double vy)
+        public void SetBallSpeedForTeam(Team team, double vx, double vy) // Устанавливка скорости мяча для заданной команды
         {
             if (team == HomeTeam)
             {
@@ -63,3 +63,4 @@ namespace Jalgpall
             Ball.Move();
         }
     }
+}
